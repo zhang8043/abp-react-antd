3f0354459ce3de95b077f03b4383b92bf11bff72
Maek Mo <80439484@qq.com>
2019-02-01 09:53:42 +0800

    添加数据字典实体

---
+++ Precise-Core/Precise.Core/DataItems/ItemsDetailEntity.cs
@@ -0,0 +1,47 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Precise.DataItems
+{
+    /// <summary>
+    /// 字典项
+    /// </summary>
+    public class ItemsDetailEntity : FullAuditedEntity<string>, IPassivable
+    {
+        /// <summary>
+        /// 分类Id
+        /// </summary>
+        public string ItemId { get; set; }
+        /// <summary>
+        /// 父级Id
+        /// </summary>
+        public string ParentId { get; set; }
+        /// <summary>
+        /// 项编号
+        /// </summary>
+        public string ItemCode { get; set; }
+        /// <summary>
+        /// 项名称
+        /// </summary>
+        public string ItemName { get; set; }
+        /// <summary>
+        /// 是否默认
+        /// </summary>
+        public bool? IsDefault { get; set; }
+        /// <summary>
+        /// 排序吗
+        /// </summary>
+        public int? SortCode { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 是否可用
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Precise-Core/Precise.Core/DataItems/ItemsEntity.cs
@@ -0,0 +1,39 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Precise.DataItems
+{
+    /// <summary>
+    /// 字典分类
+    /// </summary>
+    public class ItemsEntity : FullAuditedEntity<string>, IPassivable
+    {
+        /// <summary>
+        /// 父级Id
+        /// </summary>
+        public string ParentId { get; set; }
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public string EnCode { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// 排序码
+        /// </summary>
+        public int? SortCode { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 是否可用
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
